e1f7f7ef108b859a74d655cc254fb75a22267178
soxlo-production <soxlo.production@gmail.com>
2026-09-13 18:54:02 +0200

    Add live network-first service worker for SOXLO Messenger

---
+++ messenger-sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(key => caches.delete(key)));
+    await self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request, { cache: 'no-store' }));
+});
